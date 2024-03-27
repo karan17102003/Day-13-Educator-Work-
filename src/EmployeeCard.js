84bf141cad503f7ee02a643f7786ada5681d3dac
karan17102003 <karanmehra17102003@gmail.com>
2024-03-27 21:55:21 +0530

    first

---
+++ src/EmployeeCard.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "./EmployeeCard.css"
+
+const EmployeeCard = ({ name, age, isAdded, btnType, btnAction, id }) => {
+
+  function clickHandler() {
+    btnAction(id);
+  }
+
+  return (
+    <div className="employee-card-container">
+      <p>{name}</p>
+      <p>{age}</p>
+      {btnType === "add" ? (
+        isAdded === false && <button onClick={clickHandler}>{btnType}</button>
+      ) : (
+        <button onClick={clickHandler}>{btnType}</button>
+      )}
+    </div>
+  );
+};
+
+export default EmployeeCard;
